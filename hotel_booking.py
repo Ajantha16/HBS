05ff2e035c733495bbda9570871b7a26bebd6fef
Ajantha16 <127102910+Ajantha16@users.noreply.github.com>
2025-10-15 09:59:44 +0530

    Update hotel_booking.py

--- hotel_booking.py
+++ hotel_booking.py
@@ -1,9 +1,43 @@
-def hotel_booking_system
-def login_page():
+def hotel_booking_system():
+  def login_page():
   {
-  @login
-  print("enter username/mail_id:");
-  print("enter password":):
-  button="onclick(enter)"
+    @login
+    print("enter username/mail_id:");
+    print("enter password":);
+    button="onclick(enter)"
   }
+  def check_availability():
+   { 
+    @check_availability
+    if room_availability>=1:
+      print("room is available");
+    else:
+      print("sorry,there is no availability of rooms -now")
+    }
+  def book_rooms():
+    {
+      @room booking
+      book_room.check_availability();
+      if availability>=1 && button(book_room)=cliked:
+        print("an room is allocated - view payment page"):;
+      else:
+        print("sorry - the selected room(s) is/are not available:")
+    }
+  def checkout_page():
+    {
+    @payment
+    if rooms == selected && count >=1:
+      print("Bill generated:")
+      print("/n room charge:...,/n tax invoice details:..../n,...")
+    else:
+      print("please select rooms to proceed to payment section")
+  }
+def confirmation_page():
+  {
+    @ send mail
+    if payment == True:
+      payment.popup();
+      printf("payment successful- check mail for more details")
+    else:
+      print("payment unsuccessful ,try again");
   
