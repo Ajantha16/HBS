08d3c68b3ef4994d805412482f8eac11b538999b
Ajantha16 <127102910+Ajantha16@users.noreply.github.com>
2025-10-15 10:15:47 +0530

    Update prod.py

--- config/prod.py
+++ config/prod.py
@@ -1,1 +1,25 @@
-
+def room_type():
+  @rooms
+  def luxury_rooms():
+    self.price=room_price+tax
+    room_price=₹5000/- per day
+    tax=GST+other_taxes
+    GST=2.5%
+    other_taxes=0.5%
+    print("price of luxery_room(1):",price);
+  def premium_rooms():
+    self.p_price=p_price
+    p_price=p_room_price+tax
+    p_room_price=₹4000/- per day
+    tax=GST+other_taxes
+    GST=2.0%
+    other_taxes=0.5%
+    print("price of premium_room(1):",p_price);
+  def economic_rooms():
+    self.e_price=e_price
+    pe_price=e_room_price+tax
+    e_room_price=₹2500/- per day
+    tax=GST+other_taxes
+    GST=1.5%
+    other_taxes=0.5%
+    print("price of economic_room(1):",e_price);
